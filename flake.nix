becbaa41bbc503d49f5423c68d96337d2b50f9ee
Mathias Sven <mathiassven2@hotmail.com>
2026-05-20 20:23:51 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          packwiz
+        ];
+      };
+    };
+}
